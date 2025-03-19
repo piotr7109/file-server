d413b40fd6240e75f808c8915c6a72fbd9a2aeef
piotr7109 <piotrkrk83@gmail.com>
2025-03-19 19:37:03 +0100

    fix filename for backups

--- backupsUpload.js
+++ backupsUpload.js
@@ -5,15 +5,19 @@ const uploadBackupHandler = asyncHandler(async (req, res) => {
   const files = req.files.file;
   const file = Array.isArray(files) ? files[0] : files;
 
-  await axios.put(`${process.env.BUNNY_BACKUPS_ZONE_URL}/${file.name}`, file, {
-    params: {
-      token: process.env.BUNNY_BACKUPS_TOKEN,
-    },
-    headers: {
-      AccessKey: process.env.BUNNY_BACKUPS_ACCESS_KEY,
-      "Content-Type": "application/octet-stream",
-    },
-  });
+  await axios.put(
+    `${process.env.BUNNY_BACKUPS_ZONE_URL}/${req.query.fileName}`,
+    file.data,
+    {
+      params: {
+        token: process.env.BUNNY_BACKUPS_TOKEN,
+      },
+      headers: {
+        AccessKey: process.env.BUNNY_BACKUPS_ACCESS_KEY,
+        "Content-Type": "application/zip",
+      },
+    }
+  );
 
   res.send({ filename: file.name });
 });
